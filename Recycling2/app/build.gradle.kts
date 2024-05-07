630a3848e2489ae986dc663cc41eb86467e32b5f
ntcc10 <115433526+ntcc10@users.noreply.github.com>
2024-05-07 18:15:21 -0500

    firebase1

--- Recycling2/app/build.gradle.kts
+++ Recycling2/app/build.gradle.kts
@@ -1,6 +1,7 @@
 plugins {
     alias(libs.plugins.androidApplication)
     alias(libs.plugins.jetbrainsKotlinAndroid)
+    id("com.google.gms.google-services") version "4.4.1" apply false
 }
 
 android {

08d1055a429d6dd66dff0b073d6c9ed185a4b096
sofiamaring <159934496+sofiamaring@users.noreply.github.com>
2024-05-25 12:10:20 -0500

    Documentación AgregarProductoActivity.kt

--- Recycling_ADS/app/src/main/java/com/example/recycling/controller/AgregarProductoActivity.kt
+++ Recycling_ADS/app/src/main/java/com/example/recycling/controller/AgregarProductoActivity.kt
@@ -10,24 +10,27 @@ import com.example.recycling.model.Producto
 import com.example.recycling.model.ProductoDAO
 import com.example.recycling.R
 
+//actividad que se encarga de conectar el layout nuevo_producto con el mode
 class AgregarProductoActivity: AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.nuevo_producto)
-
+// variables para los edittext del layout
         val editTextNombre: EditText = findViewById(R.id.editTextNombre_registrar)
         val editTextTipo: EditText = findViewById(R.id.editTextTipo_registrar)
         val editTextDescripcion: EditText = findViewById(R.id.editTextDescripcion_registrar)
         val btnAgregarProducto: Button = findViewById(R.id.añadir_producto)
         val btnRegresar: ImageButton = findViewById(R.id.btnRegresar)
-
+//evento del boton
         btnAgregarProducto.setOnClickListener {
+            //recibe lo escrito en los edittext del layout
             val nombre = editTextNombre.text.toString().trim()
             val tipo = editTextTipo.text.toString().trim()
             val descripcion = editTextDescripcion.text.toString().trim()
             if (nombre.isEmpty() || tipo.isEmpty() || descripcion.isEmpty()) {
                 Toast.makeText(this, "Todos los campos son obligatorios", Toast.LENGTH_SHORT).show()
             } else {
+                //crea el producto con los dtaos ingresados para mandarselo al modelo
                 val producto = Producto(nombre, tipo, descripcion)
                 ProductoDAO.crearProducto(producto) { success, error ->
                     if (success) {
@@ -42,7 +45,7 @@ class AgregarProductoActivity: AppCompatActivity() {
 
             }
         }
-
+// boton para regresar de layout(terminar actividad)
         btnRegresar.setOnClickListener {
             // Simplemente finaliza esta actividad, regresando a la anterior en el stack
             finish()

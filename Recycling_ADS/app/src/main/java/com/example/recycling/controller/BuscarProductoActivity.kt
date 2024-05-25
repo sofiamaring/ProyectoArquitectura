5094bc055d70e50b959fd859d94219e993c075b3
sofiamaring <159934496+sofiamaring@users.noreply.github.com>
2024-05-25 12:20:07 -0500

    Documentación BuscarProductoActivity.kt

--- Recycling_ADS/app/src/main/java/com/example/recycling/controller/BuscarProductoActivity.kt
+++ Recycling_ADS/app/src/main/java/com/example/recycling/controller/BuscarProductoActivity.kt
@@ -8,12 +8,12 @@ import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import com.example.recycling.R
 import com.example.recycling.model.ProductoDAO
-
-class BuscarProductoActivity : AppCompatActivity() {
+//actividad para consultar un producto
+class BuscarProductoActivity : AppCompatActivity() { 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.busqueda_productos)
-
+        // variables para acceder a los datos del layout
         val editTextNombre: EditText = findViewById(R.id.editText_consultar_producto)
         val btnConsultarProducto: ImageButton = findViewById(R.id.btnBuscar)
         val btnRegresar: ImageButton = findViewById(R.id.btnRegresar)
@@ -23,8 +23,11 @@ class BuscarProductoActivity : AppCompatActivity() {
             if (nombre.isEmpty()) {
                 Toast.makeText(this, "Ingrese el nombre a consultar", Toast.LENGTH_SHORT).show()
             } else {
+            //una vez se llama al modelo, se trasmiten los datos a la siguiente actividad
+            //para que puedan utilizarse con el objrtivo de hacer una unica consulta y siempre estar tratando con el mismo dato consultado
                 ProductoDAO.buscarProductosPorNombre(nombre) { productos ->
                     if (productos.isNotEmpty()) {
+                        //inicializa la actividad ResultadosBusqueda y le envia los datos ingresados por el usuario
                         val intent = Intent(this, ResultadosBusquedaActivity::class.java).apply {
                             val nombres = productos.map { it.nombre }.toTypedArray()
                             val tipos = productos.map { it.tipo }.toTypedArray()

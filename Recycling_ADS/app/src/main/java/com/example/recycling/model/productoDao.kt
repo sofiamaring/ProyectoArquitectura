87eb7c058a844a44708d402a9baaef9fed5954e4
sofiamaring <159934496+sofiamaring@users.noreply.github.com>
2024-05-25 12:40:25 -0500

    Update productoDao.kt

--- Recycling_ADS/app/src/main/java/com/example/recycling/model/productoDao.kt
+++ Recycling_ADS/app/src/main/java/com/example/recycling/model/productoDao.kt
@@ -5,11 +5,15 @@ import com.google.firebase.database.DatabaseReference
 import com.google.firebase.database.DataSnapshot
 import com.google.firebase.database.DatabaseError
 import com.google.firebase.database.ValueEventListener
-
+/**
+* uso de patron singleton para la conexin global a la base de datos de los productos 
+ */
 object ProductoDAO {
     private val database: FirebaseDatabase = FirebaseDatabase.getInstance()
     private val productosRef: DatabaseReference = database.getReference("productos")
-
+    /**
+    * función para crear un objeto producto en la base de datos
+    */
     fun crearProducto(producto: Producto, callback: (Boolean, String?) -> Unit) {
         val ref = productosRef.push()
         producto.idProducto = ref.key ?: ""
@@ -22,19 +26,25 @@ object ProductoDAO {
             }
         }
     }
-
+    /**
+    * función para actualizar un objeto producto en la base de datos
+    */
     fun actualizarProducto(productoId: String, nuevoProducto: Producto, callback: (Boolean) -> Unit) {
         productosRef.child(productoId).setValue(nuevoProducto).addOnCompleteListener { task ->
             callback(task.isSuccessful)
         }
     }
-
+    /**
+    * función para eliminar un objeto producto en la base de datos
+    */
     fun eliminarProducto(productoId: String, callback: (Boolean) -> Unit) {
         productosRef.child(productoId).removeValue().addOnCompleteListener { task ->
             callback(task.isSuccessful)
         }
     }
-
+    /**
+    * función para consultar un objeto producto en la base de datos
+    */
     fun buscarProductosPorNombre(nombre: String, callback: (List<Producto>) -> Unit) {
         productosRef.orderByChild("nombre").equalTo(nombre).addListenerForSingleValueEvent(object : ValueEventListener {
             override fun onDataChange(snapshot: DataSnapshot) {
@@ -47,7 +57,9 @@ object ProductoDAO {
             }
         })
     }
-
+    /**
+    * función para obtener todos los producto de la base de datos
+    */
     fun obtenerTodosLosProductos(callback: (List<Producto>) -> Unit) {
         productosRef.addListenerForSingleValueEvent(object : ValueEventListener {
             override fun onDataChange(snapshot: DataSnapshot) {
